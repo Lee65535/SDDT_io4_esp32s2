26e77c0c224a530ffdbf83ae3aef18fa57e24c39
Kai_Xuan233 <likaiyue0989@foxmail.com>
2022-04-08 22:59:27 +0800

    pn532 lib & coin, both untested

---
+++ main/components/pn532.c
@@ -0,0 +1,5 @@
+//
+// Created by user on 2022/4/8.
+//
+
+#include "pn532.h"
---
+++ main/components/pn532.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 2022/4/8.
+//
+
+#ifndef MAIN_PN532_H
+#define MAIN_PN532_H
+
+#endif //MAIN_PN532_H
